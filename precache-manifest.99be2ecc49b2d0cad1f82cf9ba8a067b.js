d2a356c04a40f86de43cea3bf371a2da24f69366
Klemenz Hrafn <klemenz88@gmail.com>
2021-12-08 23:08:18 +0000

    Updates

---
+++ precache-manifest.99be2ecc49b2d0cad1f82cf9ba8a067b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e9748ce55d182932fb1195a2a75701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e191146d4f34d90f979c",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "93359be1299ce9b82561",
+    "url": "/static/css/main.0c422745.chunk.css"
+  },
+  {
+    "revision": "e191146d4f34d90f979c",
+    "url": "/static/js/2.f744e9ad.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/static/js/2.f744e9ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93359be1299ce9b82561",
+    "url": "/static/js/main.15754434.chunk.js"
+  },
+  {
+    "revision": "4aca8858e7392d5dcc6a",
+    "url": "/static/js/runtime-main.b3b481ca.js"
+  },
+  {
+    "revision": "2e472f7725daee3e4e6dae3106d5f0ce",
+    "url": "/static/media/header-logo.2e472f77.svg"
+  },
+  {
+    "revision": "a2f3cdcc5730cc10eef554623622b656",
+    "url": "/static/media/palms-w-logo.a2f3cdcc.png"
+  }
+]);
